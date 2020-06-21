ed9e284a79e5e0169a15449e6a0a42a7b8961525
Max Woolf <max@minimaxir.com>
2020-06-20 20:07:04 -0700

    Generate text over temperature list

--- README.md
+++ README.md
@@ -4,12 +4,20 @@ A repo containing Python scripts to automate generation from OpenAI's GPT-3 base
 
 **Disclaimer: generated content in this repository may be offensive. The READMEs of the corresponding examples will include an explicit content warning when this is the case.**
 
+Because GPT-3 favors few-shot learning, the input prompts must be longer and more semantically structured than tiny prompts often used with GPT-2. (but we will still need to test both)
+
+# Repo Layout
+
+This repo contains folders for each prompt example in the `/examples` folder.
+
+The README for each prompt example contains the input, the output at `temperature=0.0` (i.e. the model will always choose the most likely output and is therefore deterministic), and any content warnings as noted above.
+
 ## Usage
 
 The script requires the installation of certain common Python packages:
 
 ```sh
-pip3 install requests pyyaml fire
+pip3 install requests pyyaml fire tqdm
 ```
 
 ## Maintainer/Creator
--- openai_api.py
+++ openai_api.py
@@ -1,9 +1,22 @@
 import requests
 import yaml
 import json
+import logging
+from tqdm.auto import trange
+
+logger = logging.getLogger("gpt3-experiments")
+logger.setLevel(logging.INFO)
+
+logging.basicConfig(
+    format="%(asctime)s — %(levelname)s — %(name)s — %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
 
-def gpt3_generate(prompt: str) -> None:
+def gpt3_generate(
+    prompt: str = "Once upon a time", config_file: str = "config.yml",
+) -> None:
     """
     Generates texts via GPT-3 and saves them to a file.
     """
@@ -18,19 +31,26 @@ def gpt3_generate(prompt: str) -> None:
     data = {
         "prompt": prompt,
         "max_tokens": c["max_tokens"],
-        "temperature": c["temperature"],
     }
 
-    r = requests.post(
-        f"https://api.openai.com/v1/engines/{c['model']}/completions",
-        headers=headers,
-        data=json.dumps(data),
-    )
+    num_generate = c["num_generate"]
 
-    gen_text = r.json()["choices"][0]["text"]
+    for temp in c["temperatures"]:
+        output_file = f"output_{str(temp).replace('.', '_')}.txt"
+        logger.info(
+            f"Writing {num_generate} samples at temperature {temp} to {output_file}."
+        )
+        data.update({"temperature": temp})
+        with open(output_file, "w", encoding="utf-8") as f:
+            for _ in trange(num_generate):
+                r = requests.post(
+                    f"https://api.openai.com/v1/engines/{c['model']}/completions",
+                    headers=headers,
+                    data=json.dumps(data),
+                )
 
-    with open("output.txt", "w", encoding="utf-8") as f:
-        f.write(gen_text)
+                gen_text = r.json()["choices"][0]["text"]
+                f.write("{}\n{}".format(gen_text, "=" * 20 + "\n"))
 
 
 if __name__ == "__main__":
